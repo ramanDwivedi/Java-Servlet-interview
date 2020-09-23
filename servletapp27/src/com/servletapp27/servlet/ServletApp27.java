83a64f4e52d807d206f9545d39475d63203e6cd7
raman <raman@192.168.0.8>
2020-09-23 20:10:55 +0530

    ArithematicException in Servlet.

---
+++ servletapp27/src/com/servletapp27/servlet/ServletApp27.java
@@ -0,0 +1,20 @@
+package com.servletapp27.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class ServletApp27 extends HttpServlet {
+private static final long serialVersionUID = 1L;
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+PrintWriter out=response.getWriter();
+out.println(10/0);
+	
+	
+	
+	}
+
+}
