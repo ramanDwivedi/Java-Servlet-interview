c0ecffe4bb3735679760b1b168d5ab76fa605160
raman <raman@192.168.0.8>
2020-09-23 19:39:04 +0530

    Servlet Config.

---
+++ servletapp04/src/com/servletcontectapp/ServletEx02.java
@@ -0,0 +1,33 @@
+package com.servletcontectapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class ServletEx02 extends HttpServlet {
+	private static final long serialVersionUID =1l;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	response.setContentType("text/html");
+	PrintWriter out=response.getWriter();
+	ServletConfig config=getServletConfig();
+	ServletContext context=config.getServletContext();
+	out.println("a->"+context.getInitParameter("a"));
+	out.println("b->"+context.getInitParameter("b"));
+	out.println("c->"+config.getInitParameter("c"));
+	out.println("d->"+config.getInitParameter("d"));
+	out.println("e->"+config.getInitParameter("e"));
+	out.println("f->"+config.getInitParameter("f"));
+
+
+
+
+
+	
+	}
+
+}
